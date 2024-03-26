ba6570e8e2b380a0d660325bdae5aa40b57c9613
Majdi Benboubaker <majdiboubaker82@gmail.com>
2024-03-27 00:13:14 +0100

    First step : configuring the express app/ port/ routes/ middleware

--- Backend/server.js
+++ Backend/server.js
@@ -3,12 +3,28 @@
 //? *instally nodemon bch kol mata3mal changes 3al file server yauto yrefrechi
 const express = require("express");
 
+//? bch tnajm test3ml el environment variables eli mawjodin fi .env file
+require('dotenv').config();
+
+
 //? 3)tasna3 express app
 const app = express();
 
+//? middleware
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method)
+  next()
+})
+
+//? routes
+app.get("/", (req, res) =>{
+  res.json({mssg: "Welcome to the app" })
+})
+
 //? 4)lzm tasna3 port bch tnjm ta3ml listening lil requests
-app.listen(8000, () => {
-  console.log("Listening on port 8000");
+app.listen(process.env.PORT, () => {
+  console.log("Listening on port", process.env.PORT);
 });
 
 //? 5)tnjm tw sa7by tlancy server using the commande: nodemon server.js
